c6b21c2fc98c3bb7a28896c49b1d58c053975097
Yulia Kraynova <ungo1998s@yandex.ru>
2023-11-08 20:32:36 +0300

    импорты в index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './vendor/normalize.css';
-import './vendor/fonts.css'
+import './fonts/normalize.css';
+import './fonts/fonts.css'
 import './index.css';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
